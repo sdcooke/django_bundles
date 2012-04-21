c94864c926a27b4bc224a7e4ffcf4d036c56b54e
Sam Cooke <sam@mixcloud.com>
2012-04-21 16:34:55 +0100

    Adding a remove_bundles management command

---
+++ django_bundles/management/commands/remove_bundles.py
@@ -0,0 +1,23 @@
+import os
+from django.core.management.base import BaseCommand
+
+from django_bundles.conf.bundles_settings import bundles_settings
+from django_bundles.core import get_bundles, get_bundle_versions
+
+class Command(BaseCommand):
+    help = "Removes any created bundles"
+
+    def handle(self, *args, **options):
+
+        bundle_versions = get_bundle_versions()
+
+        for bundle in get_bundles():
+            hash_version = bundle_versions[bundle.name]
+            bundle_path = '%s.%s.%s' % (os.path.join(bundle.bundle_file_root, bundle.bundle_filename), hash_version, bundle.bundle_type)
+            self.stdout.write("Removing bundle: %s\n" % bundle_path)
+            os.remove(bundle_path)
+
+        self.stdout.write("Removing bundles version file: %s\n" % bundles_settings.BUNDLES_VERSION_FILE)
+        os.remove(bundles_settings.BUNDLES_VERSION_FILE)
+
+        self.stdout.write("Done.\n")
