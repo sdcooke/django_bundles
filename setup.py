50d441d16e7cc975675480ed4aac66f13d61a28d
Sam Cooke <sam@mixcloud.com>
2012-09-18 13:07:32 +0100

    Stuff for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='django-bundles',
+      version='0.2.1',
+      description='Another Django media bundler',
+      author='Sam Cooke',
+      author_email='sam.cooke@xylate.com',
+      url='https://github.com/sdcooke/django_bundles',
+      packages=find_packages(),
+      include_package_data=True,
+      license="MIT license, see LICENSE file",
+)
