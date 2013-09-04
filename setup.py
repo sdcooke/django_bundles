ce9c7bad80bc2e31135d1902997343d83a11abc6
Sam Cooke <sam@mixcloud.com>
2013-09-04 16:11:33 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='django-bundles',
-      version='0.3.2',
+      version='0.3.3',
       description='Another Django media bundler',
       author='Sam Cooke',
       author_email='sam.cooke@xylate.com',
