db1f3aaa5147f1e5c75d86b7cbf32830aa501e0c
Mat Clayton <mat@wakari.co.uk>
2017-01-18 15:22:02 +0000

    make tests work!

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+tests_require = [
+    'Django>=1.4,<1.11',
+    'tox>=2.3.1,<3.0.0',
+]
+
 setup(name='django-bundles',
       version='0.6.6',
       description='Another Django media bundler',
@@ -10,4 +15,6 @@ setup(name='django-bundles',
       packages=find_packages(),
       include_package_data=True,
       license="MIT license, see LICENSE file",
+      tests_require=tests_require,
+      test_suite="testrunner.runtests",
 )
---
+++ testrunner.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+from django.conf import settings
+import django
+
+
+def runtests():
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, test_dir)
+
+    settings.configure(
+        DEBUG=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+            }
+        },
+        INSTALLED_APPS=('django.contrib.auth',
+                        'django.contrib.contenttypes',
+                        'django.contrib.sessions',
+                        'django.contrib.admin',
+                        'django_bundles',
+                        'tests'),
+        ROOT_URLCONF='tests.urls',
+        MIDDLEWARE_CLASSES = (
+            'django.contrib.sessions.middleware.SessionMiddleware',
+            'django.middleware.csrf.CsrfViewMiddleware',
+            'django.contrib.auth.middleware.AuthenticationMiddleware',
+            'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        ),
+        TEMPLATES = [
+            {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [],
+                'APP_DIRS': True,
+                'OPTIONS': {},
+            },
+        ],
+    )
+    django.setup()
+
+    from django.test.utils import get_runner
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(verbosity=1, failfast=False)
+    failures = test_runner.run_tests(['tests', ])
+    sys.exit(bool(failures))
+
+
+if __name__ == '__main__':
+    runtests()
