958f73c2f6ea136a15e05f8e60c28fc56f17c074
Mat Clayton <mat@wakari.co.uk>
2017-01-18 15:22:38 +0000

    delete nuked code

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# from django_bundles.tests.utils import *
-# from django_bundles.tests.utils.files import *
-# from django_bundles.tests.utils.processes import *
-#
-# from django_bundles.tests.conf import *
-#
-# from django_bundles.tests.core import *
-#
-# from django_bundles.tests.templatetags import *
